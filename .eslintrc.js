12868f4174c4707a9bd948df5a82542cec267449
scaleflake <scaleflake@gmail.com>
2020-06-27 05:27:32 +0300

    Do first steps

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,5 +13,30 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'max-len': [2, 120],
+    'func-names': [0],
+    'no-lonely-if': [0],
+    'no-param-reassign': [0],
+    'no-restricted-syntax': [
+      'error',
+      {
+        selector: 'ForInStatement',
+        message: 'for..in loops iterate over the entire prototype chain, which is virtually never'
+        + ' what you want. Use Object.{keys,values,entries}, and iterate over the resulting array.',
+      },
+      // { NOTE: its okay
+      //   selector: 'ForOfStatement',
+      //   message: 'iterators/generators require regenerator-runtime, which is too heavyweight for'
+      //   + ' this guide to allow them. Separately, loops should be avoided in favor of array iterations.',
+      // },
+      {
+        selector: 'LabeledStatement',
+        message: 'Labels are a form of GOTO; using them makes code confusing and hard to maintain and understand.',
+      },
+      {
+        selector: 'WithStatement',
+        message: '`with` is disallowed in strict mode because it makes code impossible to predict and optimize.',
+      },
+    ],
   },
 };
--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,183 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div :class="s.wrapper">
+    <div :class="s.menu">
+      <PackageItem
+        :package-name="currentPackageName"
+      />
+      <div :class="s.submenu" style="padding-top: 5px">
+        <PackageItem
+          v-for="(_, packageName) in stats.packages"
+          :key="packageName"
+          :package-name="packageName"
+        />
+      </div>
+    </div>
+    <div :class="s.editor" ref="editor" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import lo from 'lodash';
+import Vue from 'vue';
+import axios from 'axios';
+import * as monaco from 'monaco-editor';
+
+const PackageItem = Vue.extend({
+  props: {
+    packageName: String,
+  },
+  computed: {
+    packageStats() {
+      return this.$parent.getPackageStats(this.packageName);
+    },
+  },
+  render() {
+    const { packageName, packageStats } = this;
+    const k = Math.ceil((packageStats.diffCount / packageStats.totalCount) * 1000) / 10;
+    return (
+      <div>
+        <span
+          style={{
+            display: 'inline-block',
+            width: '400px',
+            cursor: 'pointer',
+            color: packageName === this.$parent.currentPackageName ? '#66ffff' : 'unset',
+          }}
+          vOn:click={() => this.$parent.selectPackage(packageName)}
+        >{packageName}</span>
+        <span style="color: lightsteelblue">{` ${packageStats.totalCount}`}</span>
+        <span style="color: orange">{` ${packageStats.diffCount}`}</span>
+        <span style={{ color: k > 0 ? '#9d75cf' : '#aaaaaa' }}>
+          {` (${k}%)`}
+        </span>
+      </div>
+    );
+  },
+});
+
+const baseUrl = 'http://localhost:8081';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    PackageItem,
+  },
+  data() {
+    return {
+      stats: null,
+    };
+  },
+  computed: {
+    currentPackageName() {
+      return window.location.pathname.replace('/', '');
+    },
+  },
+  methods: {
+    getPackageStats(packageName) {
+      return this.stats?.packages[packageName] || {};
+    },
+    selectPackage(packageName) {
+      window.location.href = `/${packageName}`;
+    },
+  },
+  async mounted() {
+    (async () => {
+      const { data } = await axios.get(`${baseUrl}`);
+      this.stats = data;
+    })();
+
+    const { currentPackageName: p } = this;
+
+    const { data: packages } = await axios.get(`${baseUrl}/packages`);
+
+    if (!packages.includes(p)) {
+      this.selectPackage(packages[0]);
+    }
+
+    monaco.editor.defineTheme('myTheme', {
+      base: 'vs-dark',
+      inherit: true,
+      rules: [],
+      colors: {
+        // #1e1e1e
+        'diffEditor.insertedTextBackground': '#423324',
+        'diffEditor.removedTextBackground': '#381e39',
+      },
+    });
+
+    const diffEditor = monaco.editor.createDiffEditor(this.$refs.editor, {
+      originalEditable: true,
+      automaticLayout: true,
+      theme: 'myTheme',
+    });
+
+    const { data } = await axios.get(`${baseUrl}/${p}`);
+    const [pathA, codeA] = Object.entries(data['beautyagent-backend-shop'])[0];
+    const [pathB, codeB] = Object.entries(data['beautyagent-backend-shop-parser'])[0];
+
+    diffEditor.setModel({
+      original: monaco.editor.createModel(codeA, pathA.endsWith('.js') ? 'javascript' : 'typescript'),
+      modified: monaco.editor.createModel(codeB, pathB.endsWith('.js') ? 'javascript' : 'typescript'),
+    });
+
+    const editorA = diffEditor.getOriginalEditor();
+    editorA.onDidChangeModelContent(lo.debounce(async () => {
+      await axios.post(`${baseUrl}/${p}`, {
+        'beautyagent-backend-shop': {
+          [pathA]: editorA.getValue(),
+        },
+      });
+    }, 300));
+
+    const editorB = diffEditor.getModifiedEditor();
+    editorB.onDidChangeModelContent(lo.debounce(async () => {
+      await axios.post(`${baseUrl}/${p}`, {
+        'beautyagent-backend-shop-parser': {
+          [pathB]: editorB.getValue(),
+        },
+      });
+    }, 300));
   },
 };
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style module="s" lang="scss">
+.wrapper {
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-flow: column;
+}
+.menu {
+  width: 100%;
+  position: relative;
+}
+.submenu {
+  position: absolute;
+  z-index: 1;
+  background: #101010;
+  overflow: hidden;
+  width: 100%;
+  transform: scaleY(0);
+  transform-origin: top;
+  transition: transform 0.2s ease;
+}
+.menu:hover .submenu {
+  transform: scaleY(1);
+}
+
+.editor {
+  width: 100%;
+  flex: 1;
+}
+</style>
+
+<style>
+body {
+  margin: 0;
+  background: #101010;
+  color: #cecece;
+  font-family: monospace;
+  font-size: 15px;
 }
 </style>
